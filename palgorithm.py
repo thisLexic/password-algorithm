cdd4b5db538d9a75fa3cc28d949af30e0b4a5da3
thisLexic <benvillabroza1999@gmail.com>
2020-11-20 15:19:55 +0800

    🔑 Edit Password Algorithm - Make Secruity Level limited to 1 and 2. Make returned password all alphanumeric characters

--- palgorithm.py
+++ palgorithm.py
@@ -1,4 +1,5 @@
 from hashlib import sha512
+from map import map
 
 
 def main():
@@ -8,7 +9,7 @@ def main():
     """
     inputList = getValues()
     sha512Hash = sha512HashUserInput(inputList)
-    securityLevel = int(input('Security: '))
+    securityLevel = int(input('Security Level: '))
     myPassword = pAlgorithm(sha512Hash, securityLevel)
     print(myPassword)
 
@@ -19,11 +20,14 @@ def getValues():
     website = website to be used for (ex: Facebook, Github, Gmail)
     username = username/email/id for the account (ex: hackerman, hack@gmail.com, djangoUbuntu18AWSServer)
     secret = password. This stays the same for any password in any given website
+    breaches = number of times your password for this account has been breached, this can be stored externally
     """
+    print("=== Executing Palgorithm ===")
     website = input('Website: ')
     username = input('Username: ')
     secret = input('Secret: ')
-    return [website, username, secret]
+    breaches = input('Breaches: ')
+    return [website, username, secret, breaches]
 
 
 def sha512HashUserInput(inputList):
@@ -52,13 +56,11 @@ def pAlgorithm(sha512Hash, securityLevel):
     Password Algorithm
     returns your ACTUAL password
     Any algorithm that returns your password
-
     Must be simple enough so you won't forget
     Must be difficult enough so no one else can get it
     Must be deterministic
-
     securityLevel determines the length of the password to be returned
-    Valid securityLevel in this algorithm is from 1 to 4
+    Valid securityLevel in this algorithm is from 1 to 2
     
     The following succeeding code is an example only
     Use your own Password Algorithm for security
@@ -66,17 +68,31 @@ def pAlgorithm(sha512Hash, securityLevel):
     sectionsha512Hash = sha512Hash[22:70]
     inverted512Hash = sha512Hash[::-1]
     myLongPassword = sha512Hex(sectionsha512Hash + " hello world " + inverted512Hash)
+    myShorterPassword = alphanumeric(myLongPassword)
     if securityLevel == 1:
-        length = 6
-    elif securityLevel == 2:
-        length = 10
-    elif securityLevel == 3:
         length = 20
-    elif securityLevel == 4:
+    elif securityLevel == 2:
         length = 128
     else:
-        raise RuntimeError("Security Level must be 1, 2, 3, or 4")
-    myPassword = myLongPassword[:length]
+        raise RuntimeError("Security Level must be 1 or 2")
+    myPassword = myShorterPassword[:length]
     return myPassword
+
+def alphanumeric(longHex):
+    longHexList = getHexList(longHex)
+    alphanumeric = ""
+    for h in longHexList:
+        alphanumeric += map[h]
+    return alphanumeric
+
+def getHexList(longHex):
+    longHexList = []
+    char = ""
+    for h in longHex:
+        char += h
+        if len(char) == 2:
+            longHexList.append(char)
+            char = ""
+    return longHexList
 
 main()
